8ed321c197dcc77d49c371ffa3bb4f511b2d2e7b
Hailey <foxwavez@gmail.com>
2017-11-09 20:05:33 +0900

    Update entry_repository.rb
    change text

--- lib/learn_teach_hanami/repositories/entry_repository.rb
+++ lib/learn_teach_hanami/repositories/entry_repository.rb
@@ -2,4 +2,8 @@ class EntryRepository < Hanami::Repository
   def most_recent
     entries.order { created_at.desc }.call
   end
+  
+  defleast_recent
+    entries.order { created_at.desc }.call
+  end
 end
